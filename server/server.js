0c4a95c57657346974538f2d19ee0b95e9d72e64
Yasaswini <yasa2005.04@gmail.com>
2025-08-22 22:13:50 +0530

    socket server added

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { createServer } from "http";        
+import { Server } from "socket.io";  
 
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
-import userRouter from './routes/userroutes.js';
+import userRouter from './routes/userRoutes.js';
 import groupRouter from './routes/groupRoutes.js';
 import taskRouter from './routes/taskRoutes.js';
 
@@ -13,6 +15,19 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Create http server
+const server = createServer(app);
+
+// Socket.io setup
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST"]
+  }
+});
+
 
 // Connect to MongoDB
 connectDB();
@@ -22,6 +37,7 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -36,6 +52,14 @@ app.get("/", (req, res) => {
   res.send(" StudySync API is running...");
 });
 
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+  
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -45,6 +69,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () =>
+server.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
